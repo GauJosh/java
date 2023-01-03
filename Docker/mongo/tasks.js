77b024f520cff04a4bec76a945e328ace74b2263
Gaurav Joshi <gaurav.joshi@runbuggy.com>
2023-01-03 11:18:11 -0700

    Update tasks.js

--- Docker/mongo/tasks.js
+++ Docker/mongo/tasks.js
@@ -1,10 +1,11 @@
-print(db.getCollection('view').updateMany({_id: {$in: ['t-available', 't-unassigned']}}, {$set: {"roles": [
-    "RUNBUGGY_ADMIN",
-    "RUNBUGGY_OPERATIONS",
-    "RUNBUGGY_MANAGER",
-    "RUNBUGGY_SUPER_ADMIN",
-    "RUNBUGGY_SYSTEM",
-    "TRANSPORTER_ADMIN",
-    "TRANSPORTER_USER",
-    "TRANSPORTER_TRIAL_ADMIN"
-  ]}}))
+db.getCollection('view').updateMany({_id: {$in: ['t-available', 't-unassigned']}}, {$set: {"roles": [
+         "RUNBUGGY_ADMIN",
+         "RUNBUGGY_OPERATIONS",
+         "RUNBUGGY_MANAGER",
+         "RUNBUGGY_SUPER_ADMIN",
+         "RUNBUGGY_SYSTEM",
+         "TRANSPORTER_ADMIN",
+         "TRANSPORTER_USER",
+         "TRANSPORTER_TRIAL_USER",
+         "TRANSPORTER_TRIAL_ADMIN"
+       ]}})
